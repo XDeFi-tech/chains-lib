869a72473c685b1bee137554019279149e764995
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2022-04-23 16:47:20 +0300

    chore: require derivation path for sign method

--- apps/core/src/core/signer/signer.provider.ts
+++ apps/core/src/core/signer/signer.provider.ts
@@ -2,11 +2,11 @@ import { Injectable } from '../../common';
 import { Msg } from '../msg';
 
 @Injectable()
-export abstract class Provider {
+export abstract class Provider<S = any> {
   protected key?: string;
 
   abstract getAddress(derivation: string): Promise<string>;
-  abstract sign(msgs: Msg.Data): Promise<string>;
+  abstract sign(derivation: string, msgs: Msg.Data): Promise<S>;
   abstract verifyAddress(address: string): boolean;
 
   withPhrase(value: string) {
