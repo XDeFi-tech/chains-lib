ad6d58ad79b5d79ac693898a3cf986e84a69723e
Danil Kuchmenko <danylo@xdefi.io>
2023-03-15 17:50:01 +0200

    fix: eslint fixes

--- packages/solana/src/chain.provider.ts
+++ packages/solana/src/chain.provider.ts
@@ -1,64 +1,64 @@
 import {
-    DataSource,
-    Chain,
-    ChainDecorator,
-    Coin,
-    GasFee,
-    GasFeeSpeed,
-    Msg,
-    MsgData,
-    Response,
-    Transaction,
-    Balance,
-    FeeData,
-} from '@xdefi/chains-core';
-import 'reflect-metadata';
-import { ChainMsg } from './msg';
+  DataSource,
+  Chain,
+  ChainDecorator,
+  Coin,
+  GasFee,
+  GasFeeSpeed,
+  Msg,
+  MsgData,
+  Response,
+  Transaction,
+  Balance,
+  FeeData,
+} from '@xdefi/chains-core'
+import 'reflect-metadata'
+import { ChainMsg } from './msg'
 
 @ChainDecorator('SolanaProvider', {
   deps: [],
   providerType: 'Solana',
 })
 export class SolanaProvider extends Chain.Provider {
-    constructor(
-        dataSource: DataSource,
-        options?: Chain.IOptions,
-    ) {
-        super(dataSource, options);
-        // this.rpcProvider = ;
-    }
+  constructor(dataSource: DataSource, options?: Chain.IOptions) {
+    super(dataSource, options)
+    // this.rpcProvider = ;
+  }
 
-    createMsg(data: MsgData): Msg {
-        return new ChainMsg(data);
-    }
+  createMsg(data: MsgData): Msg {
+    return new ChainMsg(data)
+  }
 
-    async getTransactions(address: string, afterBlock?: number | string): Promise<Response<Transaction[], Transaction>> {
-        return new Response(
-            () => this.dataSource.getTransactions({ address, afterBlock}),
-            () => this.dataSource.subscribeTransactions({ address })
-        );
-    }
+  async getTransactions(
+    address: string,
+    afterBlock?: number | string
+  ): Promise<Response<Transaction[], Transaction>> {
+    return new Response(
+      () => this.dataSource.getTransactions({ address, afterBlock }),
+      () => this.dataSource.subscribeTransactions({ address })
+    )
+  }
 
-    async estimateFee(msgs: Msg[], speed: GasFeeSpeed): Promise<FeeData[]> {
-        throw new Error('Method not implemented.');
-    }
+  async estimateFee(msgs: Msg[], speed: GasFeeSpeed): Promise<FeeData[]> {
+    throw new Error('Method not implemented.')
+  }
 
-    async getBalance(address: string): Promise<Response<Coin[], Balance[]>> {
-        return new Response(
-            () => this.dataSource.getBalance({ address }),
-            () => this.dataSource.subscribeBalance({ address })
-        );
-    }
+  async getBalance(address: string): Promise<Response<Coin[], Balance[]>> {
+    return new Response(
+      () => this.dataSource.getBalance({ address }),
+      () => this.dataSource.subscribeBalance({ address })
+    )
+  }
 
-    async gasFeeOptions(): Promise<GasFee> {
-        throw new Error('Method not implemented.');
-    }
+  async gasFeeOptions(): Promise<GasFee> {
+    throw new Error('Method not implemented.')
+  }
 
-    async getNonce(address: string): Promise<number> {
-        throw new Error('Method not implemented.');
-    }
+  async getNonce(address: string): Promise<number> {
+    throw new Error('Method not implemented.')
+  }
 
-    async broadcast(msgs: Msg[]): Promise<Transaction[]> {
-        throw new Error('Method not implemented.');
-    }
+  async broadcast(msgs: Msg[]): Promise<Transaction[]> {
+    throw new Error('Method not implemented.')
+  }
 }
