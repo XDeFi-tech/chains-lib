74bb26350341c8f8ded683c84a756546c8bc4416
Danil Kuchmenko <danylo@xdefi.io>
2023-05-02 14:02:39 +0300

    chore: add expo example

---
+++ examples/expo-example/trust-wallet.signer.js
@@ -0,0 +1,32 @@
+import { Signer, SignerDecorator } from '@xdefi/chains-core';
+// import * as ExpoTrustWallet from 'expo-trust-wallet';
+
+@SignerDecorator(Signer.SignerType.TRUST_WALLET)
+export class TrustWalletSigner extends Signer.Provider {
+  getAddress(derivation) {
+    throw new Error('Method not implemented.');
+  }
+  async sign(derivation, msg) {
+    console.log('signing with TrustWallet', msg);
+    const tx = await msg.buildTx();
+    console.log('tx', tx);
+    // const signedTx = ExpoTrustWallet.signTransaction(
+    //   ExpoTrustWallet.COINS.evm,
+    //   {
+    //     ...tx,
+    //     toAddress: tx.to,
+    //     amount: '0x01',
+    //     chainId: '0x01',
+    //     gasLimit: '0x5208',
+    //     gasPrice: '0x3b9aca00',
+    //     maxFeePerGas: '0x00',
+    //     maxPriorityFeePerGas: '0x00',
+    //   }
+    // );
+    msg.sign('asdkjadjasodjoajfowjf');
+  }
+  verifyAddress(address) {
+    return true
+    // return ExpoTrustWallet.verifyAddress(address, ExpoTrustWallet.COINS.evm);
+  }
+}
