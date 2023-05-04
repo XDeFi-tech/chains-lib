3a1c1182b70cdd2b52832df8743a2903933cf7e7
Danil Kuchmenko <danylo@xdefi.io>
2023-05-04 22:47:05 +0300

    chore: add private key signer and msg

--- packages/cosmos/src/msg.ts
+++ packages/cosmos/src/msg.ts
@@ -1,12 +1,18 @@
 import { Msg as BasMsg } from '@xdefi/chains-core';
 import BigNumber from 'bignumber.js';
+import { MsgSend, coin, StdFee } from '@cosmjs/launchpad';
 
 export interface MsgBody {
   from: string;
+  to: string;
+  amount: string;
+  denom: string;
 }
 
 export interface TxData {
-  from: string;
+  msgs: MsgSend[];
+  fee: StdFee;
+  memo?: string;
 }
 
 export interface FeeEstimation {
@@ -21,8 +27,25 @@ export class ChainMsg extends BasMsg<MsgBody, TxData, FeeEstimation> {
     return this.data;
   }
 
-  buildTx() {
-    return this.data;
+  async buildTx() {
+    const msgData = this.toData();
+    const msgToSend: MsgSend = {
+      type: 'cosmos-sdk/MsgSend',
+      value: {
+        from_address: msgData.from,
+        to_address: msgData.to,
+        amount: [coin(msgData.amount, msgData.denom)],
+      },
+    };
+    const fee = {
+      amount: [coin('5000', 'uatom')],
+      gas: '890000',
+    };
+    return {
+      msgs: [msgToSend],
+      fee: fee,
+      memo: undefined,
+    };
   }
 
   getFee() {
--- packages/cosmos/src/signers/private-key.signer.ts
+++ packages/cosmos/src/signers/private-key.signer.ts
@@ -1,22 +1,41 @@
 import { Signer, SignerDecorator } from '@xdefi/chains-core';
+import { SigningCosmosClient, Secp256k1HdWallet } from '@cosmjs/launchpad';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { bech32 } from 'bech32';
 
 import { ChainMsg } from '../msg';
 
 @SignerDecorator(Signer.SignerType.PRIVATE_KEY)
 export class PrivateKeySigner extends Signer.Provider {
-  verifyAddress(_address: string): boolean {
-    throw new Error('Method not implemented.');
+  verifyAddress(address: string): boolean {
+    try {
+      const result = bech32.decode(address);
+      return result.prefix === 'cosmos' && result.words.length === 32;
+    } catch (err) {
+      return false;
+    }
   }
 
   async getAddress(privateKey: string): Promise<string> {
-    if (!this.verifyAddress(privateKey)) {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(privateKey);
+    const [{ address }] = await wallet.getAccounts();
+    if (!this.verifyAddress(address)) {
       throw new Error('Invalid address');
     }
-    throw new Error('Method not implemented.');
+    return address;
   }
 
-  async sign(_privateKey: string, _msg: ChainMsg): Promise<void> {
-    throw new Error('Method not implemented.');
+  async sign(privateKey: string, msg: ChainMsg): Promise<void> {
+    const txData = await msg.buildTx();
+    const wallet = await Secp256k1HdWallet.fromMnemonic(privateKey);
+    const [{ address: senderAddress }] = await wallet.getAccounts();
+    const client = new SigningCosmosClient(
+      msg.provider?.manifest.rpcURL as string,
+      senderAddress,
+      wallet
+    );
+    const signedTx = await client.sign(txData.msgs, txData.fee, txData.memo);
+    msg.sign(signedTx);
   }
 }
 
