42493d4e7be77fdad9c78933579d4eeca5e6395e
Danil Kuchmenko <danylo@xdefi.io>
2023-05-05 23:19:42 +0300

    chore: add ledger signer

---
+++ packages/cosmos/src/signers/ledger.signer.ts
@@ -0,0 +1,75 @@
+import App from '@ledgerhq/hw-app-eth';
+import Transport from '@ledgerhq/hw-transport-webhid';
+import { Signer, SignerDecorator } from '@xdefi/chains-core';
+import { SigningStargateClient, GasPrice } from '@cosmjs/stargate';
+import { LedgerSigner as CosmosLedgerSigner } from '@cosmjs/ledger-amino';
+import { stringToPath } from '@cosmjs/crypto';
+import { bech32 } from 'bech32';
+
+import { ChainMsg } from '../msg';
+
+@SignerDecorator(Signer.SignerType.LEDGER)
+export class LedgerSigner extends Signer.Provider {
+  verifyAddress(address: string): boolean {
+    try {
+      const result = bech32.decode(address);
+      return result.prefix === 'cosmos' && result.words.length === 32;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async getAddress(derivation: string): Promise<string> {
+    const transport = await Transport.create();
+    try {
+      const app = new App(transport);
+      const address = await app.getAddress(derivation);
+
+      return address.address;
+    } catch (err) {
+      throw err;
+    } finally {
+      transport.close();
+    }
+  }
+
+  async sign(derivation: string, msg: ChainMsg): Promise<void> {
+    const transport = await Transport.create();
+    try {
+      const signer = new CosmosLedgerSigner(transport, {
+        testModeAllowed: true,
+        hdPaths: [stringToPath(derivation)],
+        prefix: msg.provider?.manifest.chain,
+      });
+      const msgData = msg.toData();
+      const txData = await msg.buildTx();
+      const client = await SigningStargateClient.connectWithSigner(
+        msg.provider?.manifest.chain as string,
+        signer,
+        {
+          broadcastTimeoutMs: 600_000,
+          broadcastPollIntervalMs: 5_000,
+          gasPrice: GasPrice.fromString(
+            `2500${msg.provider?.manifest.chainSymbol}`
+          ),
+        }
+      );
+      const signature = await client.sign(
+        msgData.from,
+        txData.msgs.map((msg) => ({
+          typeUrl: '/cosmos.tx.v1beta1.TxBody',
+          value: msg,
+        })),
+        txData.fee,
+        txData.memo || ''
+      );
+      msg.sign(signature);
+    } catch (err) {
+      throw err;
+    } finally {
+      transport.close();
+    }
+  }
+}
+
+export default LedgerSigner;
