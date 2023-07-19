00702844a010cb966747d7661ba0d955498fd2da
Danil Kuchmenko <danylo@xdefi.io>
2023-07-19 17:49:47 +0300

    chore: add getFee to cosmos chain message

--- packages/cosmos/src/msg.ts
+++ packages/cosmos/src/msg.ts
@@ -1,5 +1,15 @@
-import { Msg as BasMsg, NumberIsh } from '@xdefi-tech/chains-core';
+import {
+  FeeEstimation,
+  GasFeeSpeed,
+  Msg as BasMsg,
+  MsgData,
+  NumberIsh,
+} from '@xdefi-tech/chains-core';
 import { StdTx } from '@cosmjs/amino';
+import BigNumber from 'bignumber.js';
+
+import { CosmosManifest } from './manifests';
+import type { CosmosProvider } from './chain.provider';
 
 export interface MsgBody {
   from: string;
@@ -18,9 +28,18 @@ export interface TxData {
 
 export class ChainMsg extends BasMsg<MsgBody, TxData> {
   signedTransaction: StdTx | undefined;
+  manifest: CosmosManifest | undefined;
 
   public toData() {
     return this.data;
+  }
+
+  constructor(
+    public readonly data: MsgData,
+    public readonly provider?: CosmosProvider
+  ) {
+    super(data, provider);
+    this.manifest = provider?.manifest as CosmosManifest;
   }
 
   async buildTx() {
@@ -42,8 +61,32 @@ export class ChainMsg extends BasMsg<MsgBody, TxData> {
     };
   }
 
-  getFee() {
-    return this.data;
+  async getFee(speed?: GasFeeSpeed) {
+    const data = this.toData();
+    const estimation: FeeEstimation = {
+      fee: null,
+      maxFee: null,
+    };
+
+    if (!data.gasLimit && !data.gasPrice && this.provider) {
+      const [feeEstimation] = await this.provider.estimateFee(
+        [this],
+        speed || GasFeeSpeed.medium
+      );
+      if (feeEstimation.gasPrice && feeEstimation.gasLimit) {
+        estimation.fee = BigNumber(feeEstimation.gasLimit.toString())
+          .multipliedBy(feeEstimation.gasPrice.toString())
+          .dividedBy(10 ** (this.manifest?.decimals || 0))
+          .toString();
+      }
+    } else if (data.gasLimit && data.gasPrice) {
+      estimation.fee = BigNumber(data.gasLimit)
+        .multipliedBy(data.gasPrice)
+        .dividedBy(10 ** (this.manifest?.decimals || 0))
+        .toString();
+    }
+
+    return estimation;
   }
 
   static fromData(data: MsgBody) {
