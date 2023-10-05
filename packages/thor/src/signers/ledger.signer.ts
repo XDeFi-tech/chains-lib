60a77b302c320a532332edd9759dce1372cdf26b
Sam Sheffres <ssheffres@gmail.com>
2023-10-05 17:30:07 +0700

    fix: throw error when asking for thorchain ledger private key instead of returning error

--- packages/thor/src/signers/ledger.signer.ts
+++ packages/thor/src/signers/ledger.signer.ts
@@ -16,7 +16,7 @@ export class LedgerSigner extends Signer.Provider {
   }
 
   async getPrivateKey(_derivation: string) {
-    return new Error('Cannot extract private key from Ledger device');
+    throw new Error('Cannot extract private key from Ledger device');
   }
 
   async getAddress(derivation: string): Promise<string> {
