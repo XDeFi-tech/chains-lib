e0d5ae677a7c3e76bab3a9fca84b8b44528663e1
Sam Sheffres <ssheffres@gmail.com>
2023-10-25 09:57:06 +0700

    feat: add litecoin ledger signer

--- packages/litecoin/src/signers/ledger.signer.ts
+++ packages/litecoin/src/signers/ledger.signer.ts
@@ -1,73 +1,111 @@
-import App from '@ledgerhq/hw-app-btc';
+import BtcOld from '@ledgerhq/hw-app-btc';
 import Transport from '@ledgerhq/hw-transport-webhid';
 import { Signer, SignerDecorator } from '@xdefi-tech/chains-core';
 import { UTXO } from '@xdefi-tech/chains-utxo';
 import * as Litecoin from 'bitcoinjs-lib';
+import { CreateTransactionArg } from '@ledgerhq/hw-app-btc/lib/createTransaction';
+import { utils } from '@xdefi-tech/chains-core';
 
 import { ChainMsg } from '../msg';
 
 @SignerDecorator(Signer.SignerType.LEDGER)
 export class LedgerSigner extends Signer.Provider {
+  network = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+  };
+
   verifyAddress(address: string): boolean {
-    if (new RegExp(/^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}$/).test(address)) {
+    try {
+      Litecoin.address.toOutputScript(address, this.network);
       return true;
-    } else {
+    } catch (err) {
       return false;
     }
   }
 
-  async getAddress(
-    derivation: string,
-    type: 'legacy' | 'p2sh' | 'bech32' | 'bech32m' | 'cashaddr' = 'legacy'
-  ): Promise<string> {
+  async getAddress(derivation: string): Promise<string> {
     const transport = await Transport.create();
-    const app = new App({ transport, currency: 'bitcoin' });
+    try {
+      const app = new BtcOld({ transport, currency: 'litecoin' });
 
-    const address = await app.getWalletPublicKey(derivation, { format: type });
-    transport.close();
+      const { bitcoinAddress } = await app.getWalletPublicKey(derivation);
 
-    return address.bitcoinAddress;
+      if (bitcoinAddress) {
+        return bitcoinAddress;
+      } else {
+        throw new Error('Error getting Litecoin address');
+      }
+    } catch (e) {
+      throw e;
+    } finally {
+      await transport.close();
+    }
   }
 
   async sign(msg: ChainMsg, derivation: string) {
     const transport = await Transport.create();
-    const app = new App({ transport, currency: 'bitcoin' });
-    const { inputs, outputs, compiledMemo, from } = await msg.buildTx();
-    const psbt = new Litecoin.Psbt({ network: Litecoin.networks.bitcoin });
-    psbt.addInputs(
-      inputs.map((utxo: UTXO) => ({
-        hash: utxo.hash,
-        index: utxo.index,
-        witnessUtxo: utxo.witnessUtxo,
-      }))
-    );
+    try {
+      const app = new BtcOld({ transport, currency: 'litecoin' });
+      const { inputs, outputs, from } = await msg.buildTx();
+      const psbt = new Litecoin.Psbt({ network: this.network });
 
-    // psbt add outputs from accumulative outputs
-    outputs.forEach((output: Litecoin.PsbtTxOutput) => {
-      if (!output.address) {
-        //an empty address means this is the change address
-        output.address = from;
-      }
-      if (!output.script) {
-        psbt.addOutput(output);
-      } else {
-        //we need to add the compiled memo this way to
-        //avoid dust error tx when accumulating memo output with 0 value
-        if (compiledMemo) {
-          psbt.addOutput({ script: compiledMemo, value: 0 });
+      inputs.forEach((utxo: UTXO) => {
+        psbt.addInput({
+          hash: utxo.hash,
+          index: utxo.index,
+          witnessUtxo: utxo.witnessUtxo,
+        });
+      });
+
+      outputs.forEach((output: Litecoin.PsbtTxOutput) => {
+        if (!output.address) {
+          output.address = from;
         }
-      }
-    });
-    const result = await app.signMessage(derivation, psbt.toHex());
-    await transport.close();
 
-    const v = result['v'] + 27 + 4;
+        psbt.addOutput({
+          script: Litecoin.address.toOutputScript(output.address, this.network),
+          value: output.value,
+        });
+      });
 
-    const signature = Buffer.from(
-      v.toString(16) + result['r'] + result['s'],
-      'hex'
-    ).toString('base64');
-    msg.sign(signature);
+      const outputWriter = new utils.BufferWriter();
+
+      outputWriter.writeVarInt(psbt.txOutputs.length);
+
+      psbt.txOutputs.forEach((output) => {
+        outputWriter.writeUInt64(output.value);
+        outputWriter.writeVarSlice(output.script);
+      });
+
+      const outputScriptHex = outputWriter.buffer().toString('hex');
+
+      const data: CreateTransactionArg = {
+        inputs: inputs.map((utxo: UTXO) => [
+          app.splitTransaction(utxo.txHex, true),
+          utxo.index,
+          utxo.witnessUtxo.script.toString('hex'),
+        ]),
+        associatedKeysets: [derivation],
+        outputScriptHex,
+        additionals: [],
+      };
+
+      const signedTx = await app.createPaymentTransaction(data);
+
+      msg.sign(signedTx);
+    } catch (e) {
+      throw e;
+    } finally {
+      await transport.close();
+    }
   }
 }
 
