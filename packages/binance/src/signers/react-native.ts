c208912f7661a3c702cac2d6824e45d061e67907
Danil Kuchmenko <danylo@xdefi.io>
2023-12-04 12:42:10 +0200

    feat: add seed phrase signer to react native export

--- packages/binance/src/signers/react-native.ts
+++ packages/binance/src/signers/react-native.ts
@@ -1,8 +1,9 @@
 import { Signer } from '@xdefi-tech/chains-core';
 
 import { PrivateKeySigner } from './private-key.signer';
+import { SeedPhraseSigner } from './seed-phrase.signer';
 // add and use trust wallet signer instead of PK
 
-const signers: typeof Signer.Provider[] = [PrivateKeySigner];
+const signers: typeof Signer.Provider[] = [PrivateKeySigner, SeedPhraseSigner];
 
 export default signers;
