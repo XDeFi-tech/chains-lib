e8edf54689589b00ebf558d952c8bfdbc592260c
Sam Sheffres <ssheffres@gmail.com>
2023-12-12 08:17:55 -0800

    fix: balances for tron

--- packages/tron/src/datasource/chain/chain.data-source.ts
+++ packages/tron/src/datasource/chain/chain.data-source.ts
@@ -53,60 +53,60 @@ export class ChainDataSource extends DataSource {
       `${this.manifest.dataProviderURL}/v1/accounts/${address}`
     );
 
-    // throw new Error(JSON.stringify(response.data));
-
     const tokenBalances: Record<string, string>[] = response.data.data[0].trc20
       ? response.data.data[0].trc20
       : [];
 
+    const abi = [
+      {
+        outputs: [{ type: 'uint256' }],
+        constant: true,
+        inputs: [{ name: 'who', type: 'address' }],
+        name: 'balanceOf',
+        stateMutability: 'View',
+        type: 'Function',
+      },
+      {
+        outputs: [{ type: 'bool' }],
+        inputs: [
+          { name: '_to', type: 'address' },
+          { name: '_value', type: 'uint256' },
+        ],
+        name: 'transfer',
+        stateMutability: 'Nonpayable',
+        type: 'Function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'decimals',
+        outputs: [{ name: '', type: 'uint8' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'symbol',
+        outputs: [{ name: '', type: 'string' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ];
+
+    this.rpcProvider.setAddress(address);
+
     for (let index = 0; index < tokenBalances.length; index++) {
       const token: Record<string, string> = tokenBalances[index];
 
-      const abi = [
-        {
-          outputs: [{ type: 'uint256' }],
-          constant: true,
-          inputs: [{ name: 'who', type: 'address' }],
-          name: 'balanceOf',
-          stateMutability: 'View',
-          type: 'Function',
-        },
-        {
-          outputs: [{ type: 'bool' }],
-          inputs: [
-            { name: '_to', type: 'address' },
-            { name: '_value', type: 'uint256' },
-          ],
-          name: 'transfer',
-          stateMutability: 'Nonpayable',
-          type: 'Function',
-        },
-        {
-          constant: true,
-          inputs: [],
-          name: 'decimals',
-          outputs: [{ name: '', type: 'uint8' }],
-          payable: false,
-          stateMutability: 'view',
-          type: 'function',
-        },
-        {
-          constant: true,
-          inputs: [],
-          name: 'symbol',
-          outputs: [{ name: '', type: 'string' }],
-          payable: false,
-          stateMutability: 'view',
-          type: 'function',
-        },
-      ];
-
-      this.rpcProvider.setAddress(address);
       const contractAddress = Object.keys(token)[0];
       const contract = await this.rpcProvider.contract(abi, contractAddress);
       const balance: BigNumber = await contract.balanceOf(address).call();
       const decimals: BigNumber = await contract.decimals().call();
       const symbol: string = await contract.symbol().call();
+
       coins.push(
         new Coin(
           new Asset({
@@ -116,7 +116,7 @@ export class ChainDataSource extends DataSource {
             symbol: symbol,
             native: false,
           }),
-          balance.div(new BigNumber(10).pow(decimals).toString())
+          balance.div(new BigNumber(10).pow(decimals).toString()).toString()
         )
       );
     }
