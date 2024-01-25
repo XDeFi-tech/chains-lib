e7c77f640a23ea392114116eedf1151528998b07
Danil Kuchmenko <danylo@xdefi.io>
2024-01-25 16:26:43 +0200

    feat: add seed phrase signer to export for each chain

--- packages/bitcoincash/src/signers/web.ts
+++ packages/bitcoincash/src/signers/web.ts
@@ -1,5 +1,6 @@
 import { PrivateKeySigner } from './private-key.signer';
 import TrezorSigner from './trezor.signer';
 import LedgerSigner from './ledger.signer';
+import { SeedPhraseSigner } from './seed-phrase.signer';
 
-export { PrivateKeySigner, TrezorSigner, LedgerSigner };
+export { PrivateKeySigner, LedgerSigner, TrezorSigner, SeedPhraseSigner };
--- packages/dogecoin/src/signers/web.ts
+++ packages/dogecoin/src/signers/web.ts
@@ -1,5 +1,6 @@
 import { PrivateKeySigner } from './private-key.signer';
 import TrezorSigner from './trezor.signer';
 import LedgerSigner from './ledger.signer';
+import { SeedPhraseSigner } from './seed-phrase.signer';
 
-export { PrivateKeySigner, LedgerSigner, TrezorSigner };
+export { PrivateKeySigner, LedgerSigner, TrezorSigner, SeedPhraseSigner };
