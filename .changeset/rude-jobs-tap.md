c2732db8f19ca1e9aa36573e06df0968a666f6dd
Danil Kuchmenko <danylo@xdefi.io>
2024-02-02 13:00:11 +0200

    fix: lint bump: version

---
+++ .changeset/rude-jobs-tap.md
@@ -0,0 +1,8 @@
+---
+'@xdefi-tech/chains-bitcoincash': patch
+'@xdefi-tech/chains-dogecoin': patch
+'@xdefi-tech/chains-litecoin': patch
+'@xdefi-tech/chains-bitcoin': patch
+---
+
+fix: gql queries for each UTXO chain
--- packages/bitcoincash/src/datasource/indexer/queries/balances.query.ts
+++ packages/bitcoincash/src/datasource/indexer/queries/balances.query.ts
@@ -1,5 +1,8 @@
 import { gqlClient } from '@xdefi-tech/chains-core';
-import { BitcoinCashBalanceDocument, Balance } from '@xdefi-tech/chains-graphql';
+import {
+  BitcoinCashBalanceDocument,
+  Balance,
+} from '@xdefi-tech/chains-graphql';
 
 export const getBalance = async (address: string): Promise<Array<Balance>> => {
   const response = await gqlClient.query({
