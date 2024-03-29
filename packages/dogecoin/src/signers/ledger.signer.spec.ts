28c54f978dcc59117e1586e9fc6a1605132771df
Sam Sheffres <ssheffres@gmail.com>
2024-03-29 15:53:50 +0700

    dont mock accumulate and use a real utxo

--- packages/dogecoin/src/signers/ledger.signer.spec.ts
+++ packages/dogecoin/src/signers/ledger.signer.spec.ts
@@ -38,14 +38,6 @@ jest.mock('../datasource/indexer/queries/balances.query', () => ({
   },
 }));
 
-jest.mock('coinselect/accumulative', () => ({
-  __esModule: true,
-  default: () => ({
-    inputs: [{ value: 1000, witnessUtxo: { script: '' } }],
-    outputs: [{ value: 100 }],
-  }),
-}));
-
 describe('ledger.signer', () => {
   let signer: LedgerSigner;
   let derivationPath: string;
@@ -59,14 +51,18 @@ describe('ledger.signer', () => {
       .spyOn(BlockchairDataProvider.prototype, 'scanUTXOs')
       .mockResolvedValue([
         {
-          hash: 'hash1',
-          value: 10000,
+          hash: '16120787df8f4bf06c422fe362eabd48d0ef2383e567d624f748cd40f9882cad',
+          value: 10000000,
           index: 0,
           witnessUtxo: {
-            value: 10000,
-            script: Buffer.from('0x0', 'hex'),
+            value: 10000000,
+            script: Buffer.from([
+              118, 169, 20, 198, 2, 220, 48, 138, 169, 74, 205, 117, 83, 119,
+              87, 238, 202, 121, 29, 169, 87, 228, 241, 136, 172,
+            ]),
           },
-          txHex: 'raw_transaction',
+          txHex:
+            '010000000102a95e8c5a5ebefa6431867f1e1691e5dd9cad10b2d1e8efce9b15c48c86f68d000000006b483045022100f21d3cac7d1332fdea2b33b8dd9859743b923dfa42431b49512d8dbb81acce5702204d2a009e45657147acf1b23204bc01a0d338e86c8cad458a394d933e2b3f24f20121031db6a1e76aad4f63aaea779d05a15a5619749a808c8d5b6856ae449b503e1d58ffffffff0280969800000000001976a914c602dc308aa94acd75537757eeca791da957e4f188ac700c4c05000000001976a91447542a50d8d84ed278690657e36bdda110f0181688ac00000000',
         },
       ]);
 
