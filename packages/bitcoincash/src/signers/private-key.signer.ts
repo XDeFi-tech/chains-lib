fecbfc515b86015e68643941f94542814c87eeee
sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>
2024-04-19 17:30:50 +0700

    fix: bch build cicd

--- packages/bitcoincash/src/signers/private-key.signer.ts
+++ packages/bitcoincash/src/signers/private-key.signer.ts
@@ -5,7 +5,6 @@ import * as BitcoinCash from '@psf/bitcoincashjs-lib';
 import * as Bitcoin from 'bitcoinjs-lib';
 import coininfo from 'coininfo';
 import * as bchaddr from 'bchaddrjs';
-import * as BitcoinCashLib from 'bitcoinjs-lib';
 
 import { ChainMsg } from '../msg';
 
@@ -82,11 +81,11 @@ export class PrivateKeySigner extends Signer.Provider {
     derivation?: string
   ): Promise<string> {
     const network = coininfo.bitcoincash.main.toBitcoinJS();
-    const pk = BitcoinCashLib.ECPair.fromWIF(
+    const pk = Bitcoin.ECPair.fromWIF(
       await this.getPrivateKey(derivation ?? ''),
       network
     );
-    const psbt = BitcoinCashLib.Psbt.fromHex(txHex, { network });
+    const psbt = Bitcoin.Psbt.fromHex(txHex, { network });
 
     psbt.signAllInputs(pk);
     psbt.finalizeAllInputs();
