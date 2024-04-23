3361e8f413bc728bf42a246578f30e06ef3ecd5a
HoangVD2 <hoang.vu2@sotatek.com>
2024-04-23 10:47:25 +0700

    feat: implement ADR-036

--- packages/cosmos/src/signers/seed-phrase.signer.ts
+++ packages/cosmos/src/signers/seed-phrase.signer.ts
@@ -10,6 +10,9 @@ import {
 import { utils, Wallet } from 'ethers';
 import { MnemonicKey, AccAddress, LCDClient } from '@terra-money/feather.js';
 import { encode } from 'bech32-buffer';
+import { verifyADR36Amino } from '@keplr-wallet/cosmos';
+import { StdSignature } from '@cosmjs/amino';
+import { fromBase64 } from '@cosmjs/encoding';
 
 import { ChainMsg, CosmosChainType } from '../msg';
 import { STARGATE_CLIENT_OPTIONS } from '../utils';
@@ -138,6 +141,22 @@ export class SeedPhraseSigner extends Signer.Provider {
 
       msg.sign(Buffer.from(tx.toBytes()).toString('base64'));
     }
+  }
+
+  async verifyMessage(
+    address: string,
+    data: Uint8Array,
+    signed: StdSignature
+  ): Promise<boolean> {
+    const isValid = verifyADR36Amino(
+      bech32.decode(address).prefix, // bech32 prefix
+      bech32.encode('cosmos', bech32.decode(address).words), // signer
+      data, // data sign message
+      fromBase64(signed.pub_key.value), // pubKeyBuffer
+      fromBase64(signed.signature) // signatureBuffer
+    );
+
+    return isValid;
   }
 }
 
