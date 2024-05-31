6e7392a2d0212efed442d2bb3434ce2933816585
sotatek-tyler-nguyen4 <166351995+sotatek-tyler-nguyen4@users.noreply.github.com>
2024-05-31 15:26:51 +0700

    feat: Add IBC transfer transactions (#213)
    * feat: Add IBC transfer transactions
    
    * feat: Partner APIs
    
    * feat: Add IBC transfer transactions
    
    * feat: Add IBC transfer transactions
    
    * chore: add returned type for utils functions
    
    * chore: update ibc transfer example
    
    * chore: remove skip client in provider
    
    ---------
    
    Co-authored-by: sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>

--- packages/cosmos/src/chain.provider.spec.ts
+++ packages/cosmos/src/chain.provider.spec.ts
@@ -3,12 +3,6 @@ import { CosmosProvider } from './chain.provider';
 import { IndexerDataSource } from './datasource';
 import { COSMOS_MANIFESTS } from './manifests';
 
-jest.mock('./datasource/indexer/queries/balances.query', () => ({
-  getBalance: () => {
-    return [];
-  },
-}));
-
 jest.mock('axios', () => ({
   create: jest.fn().mockReturnValue({
     get: jest.fn().mockResolvedValue({
@@ -32,40 +26,46 @@ jest.mock('axios', () => ({
   }),
 }));
 
+jest.mock('./datasource/indexer/queries/balances.query', () => ({
+  getBalance: () => {
+    return [];
+  },
+}));
 describe('chain.provider', () => {
-  let provider: CosmosProvider;
+  describe('Provider', () => {
+    let provider: CosmosProvider;
 
-  beforeEach(() => {
-    provider = new CosmosProvider(
-      new IndexerDataSource(COSMOS_MANIFESTS.cosmos)
-    );
-  });
-
-  it('createMsg(): should create message with data', () => {
-    const msg = provider.createMsg({
-      to: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
-      from: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
-      amount: 0.000001,
+    beforeEach(() => {
+      provider = new CosmosProvider(
+        new IndexerDataSource(COSMOS_MANIFESTS.cosmos)
+      );
     });
 
-    expect(msg).toBeInstanceOf(ChainMsg);
-  });
+    it('createMsg(): should create message with data', () => {
+      const msg = provider.createMsg({
+        to: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
+        from: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
+        amount: 0.000001,
+      });
 
-  it('should get a transaction from the blockchain', async () => {
-    const txData = await provider.getTransaction(
-      '0269644F832384D033D2223226A5FC25BD17A3646EE2BEF5C1F9009FD19E66B9'
-    );
-    expect(txData?.hash).toEqual(
-      '0269644F832384D033D2223226A5FC25BD17A3646EE2BEF5C1F9009FD19E66B9'
-    );
-  });
+      expect(msg).toBeInstanceOf(ChainMsg);
+    });
 
-  it('should get a balance', async () => {
-    const balance = await provider.getBalance(
-      'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4'
-    );
+    it('should get a transaction from the blockchain', async () => {
+      const txData = await provider.getTransaction(
+        '0269644F832384D033D2223226A5FC25BD17A3646EE2BEF5C1F9009FD19E66B9'
+      );
+      expect(txData?.hash).toEqual(
+        '0269644F832384D033D2223226A5FC25BD17A3646EE2BEF5C1F9009FD19E66B9'
+      );
+    });
 
-    const balanceData = await balance.getData();
-    expect(balanceData.length).toEqual(0);
+    it('should get a balance', async () => {
+      const balance = await provider.getBalance(
+        'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4'
+      );
+      const balanceData = await balance.getData();
+      expect(balanceData.length).toEqual(0);
+    });
   });
 });
--- packages/cosmos/src/utils.ts
+++ packages/cosmos/src/utils.ts
@@ -11,6 +11,10 @@ import {
   osmosisProtoRegistry,
   osmosisAminoConverters,
 } from 'osmojs/dist/codegen/osmosis/client';
+import axios from 'axios';
+
+import { COSMOS_MANIFESTS, CosmosHubChains } from './manifests';
+import { MsgBody } from './msg';
 
 /**
  * The Amino JSON representation of [MsgExecuteContract].
@@ -28,6 +32,72 @@ export interface AminoMsgExecuteContract {
     readonly msg: any;
     readonly funds: readonly Coin[];
   };
+}
+
+export interface ChainAsset {
+  denom: string;
+  chain_id: string;
+  origin_denom: string;
+  origin_chain_id: string;
+  trace: string;
+  is_cw20: boolean;
+  is_evm: boolean;
+  is_svm: boolean;
+  symbol: string;
+  name: string;
+  logo_uri: string;
+  decimals: number;
+  description: string;
+  recommended_symbol: string;
+}
+
+export interface TransferData {
+  port: string;
+  channel: string;
+  from_chain_id: string;
+  to_chain_id: string;
+  pfm_enabled: boolean;
+  supports_memo: boolean;
+  denom_in: string;
+  denom_out: string;
+  bridge_id: string;
+  smart_relay: boolean;
+  chain_id: string;
+  dest_denom: string;
+}
+
+export interface OperationData {
+  transfer: TransferData;
+  tx_index: number;
+  amount_in: string;
+  amount_out: string;
+}
+
+export interface RouteData {
+  source_asset_denom: string;
+  source_asset_chain_id: string;
+  dest_asset_denom: string;
+  dest_asset_chain_id: string;
+  amount_in: string;
+  amount_out: string;
+  operations: OperationData[];
+  chain_ids: string[];
+  does_swap: boolean;
+  estimated_amount_out: string;
+  swap_venues: any[];
+  txs_required: number;
+  usd_amount_in: string;
+  usd_amount_out: string;
+  estimated_fees: any[];
+  required_chain_addresses: string[];
+}
+
+export interface RecommendAsset {
+  destAssetDenom: string;
+  chainId: string;
+  decimals: string;
+  originDenom: string;
+  originalChainId: string;
 }
 
 export const STARGATE_CLIENT_OPTIONS = {
@@ -65,4 +135,152 @@ export const STARGATE_CLIENT_OPTIONS = {
       }),
     },
   }),
+};
+
+export const skipAxiosClient = axios.create({
+  baseURL: 'https://api.skip.money',
+});
+
+export const getIBCTokenInfo = async (
+  sourceChain: CosmosHubChains,
+  originDenom: string,
+  originalChain: CosmosHubChains
+): Promise<null | ChainAsset> => {
+  const sourceChainId = COSMOS_MANIFESTS[sourceChain].chainId;
+  const originalChainId = COSMOS_MANIFESTS[originalChain].chainId;
+  const {
+    data: {
+      chain_to_assets_map: {
+        [sourceChainId]: { assets },
+      },
+    },
+  } = await skipAxiosClient.get(
+    `/v1/fungible/assets?chain_id=${sourceChainId}&include_no_metadata_assets=true&include_cw20_assets=false&include_evm_assets=false&include_svm_assets=true`
+  );
+  return (assets as any[]).find(
+    (asset) =>
+      asset.origin_denom === originDenom &&
+      asset.origin_chain_id === originalChainId
+  );
+};
+
+export const getIBCTransferRouter = async (
+  amountIn: string,
+  sourceAssetDenom: string,
+  sourceAssetChain: CosmosHubChains,
+  destAssetDenom: string,
+  destAssetChain: CosmosHubChains
+): Promise<RouteData> => {
+  try {
+    const { data } = await skipAxiosClient.post('/v2/fungible/route', {
+      amountIn,
+      sourceAssetDenom,
+      sourceAssetChainId: COSMOS_MANIFESTS[sourceAssetChain].chainId,
+      destAssetDenom,
+      destAssetChainId: COSMOS_MANIFESTS[destAssetChain].chainId,
+      allowMultiTx: true,
+    });
+    return data;
+  } catch (error) {
+    throw new Error('Route not found');
+  }
+};
+
+export const createIBCTransferMsg = async (
+  route: RouteData,
+  userAddresses: string[]
+): Promise<MsgBody> => {
+  const {
+    source_asset_denom,
+    source_asset_chain_id,
+    dest_asset_denom,
+    dest_asset_chain_id,
+    amount_in,
+    amount_out,
+    operations,
+  } = route;
+  const bodyTx = {
+    source_asset_denom,
+    source_asset_chain_id,
+    dest_asset_denom,
+    dest_asset_chain_id,
+    amount_in,
+    amount_out,
+    operations,
+    address_list: userAddresses,
+  };
+  const { data } = await skipAxiosClient.post('v2/fungible/msgs', bodyTx);
+  const { msg, msg_type_url } = data.msgs[0].multi_chain_msg;
+  const msgConvert = JSON.parse(msg);
+  const message: MsgBody = {
+    from: userAddresses[0],
+    to: '',
+    amount: '0',
+    typeUrl: msg_type_url,
+    msgs: [
+      {
+        typeUrl: msg_type_url,
+        value: {
+          sourcePort: msgConvert.source_port,
+          sourceChannel: msgConvert.source_channel,
+          sender: msgConvert.sender,
+          token: {
+            denom: msgConvert.token.denom,
+            amount: msgConvert.token.amount,
+          },
+          receiver: msgConvert.receiver,
+          timeoutHeight: msgConvert.timeout_height
+            ? {
+                revisionNumber: msgConvert.timeout_height.revision_number ?? 0,
+                revisionHeight: msgConvert.timeout_height.revision_height ?? 0,
+              }
+            : undefined,
+          timeoutTimestamp: msgConvert.timeout_timestamp,
+          memo: msgConvert.memo,
+        },
+      },
+    ],
+  };
+  return message;
+};
+
+export const getIBCDestAsset = async (
+  sourceChain: CosmosHubChains,
+  destChain: CosmosHubChains,
+  denom: string
+): Promise<RecommendAsset> => {
+  const {
+    data: {
+      recommendation_entries: [
+        {
+          recommendations: [
+            {
+              asset: {
+                denom: destAssetDenom,
+                decimals,
+                chainId: chainId,
+                origin_denom: originDenom,
+                originalChainId,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  } = await skipAxiosClient.post('/v1/fungible/recommend_assets', {
+    requests: [
+      {
+        source_asset_chain_id: COSMOS_MANIFESTS[sourceChain].chainId,
+        dest_chain_id: COSMOS_MANIFESTS[destChain].chainId,
+        source_asset_denom: denom,
+      },
+    ],
+  });
+  return {
+    destAssetDenom,
+    chainId,
+    decimals,
+    originDenom,
+    originalChainId,
+  };
 };
