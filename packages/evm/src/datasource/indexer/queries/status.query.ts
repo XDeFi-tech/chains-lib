69dd11d9e0149f54d2dddf36b61daaf876039080
danilxdefi <109511564+danilxdefi@users.noreply.github.com>
2024-06-07 15:21:33 +0300

    feat: xdefi-7902 evm cache (#233)
    * feat: add separate queries for each EVM chain
    
    * fix: lint
    
    * fix: improve error message

--- packages/evm/src/datasource/indexer/queries/status.query.ts
+++ packages/evm/src/datasource/indexer/queries/status.query.ts
@@ -1,30 +1,65 @@
-import { gql } from 'graphql-tag';
 import { gqlClient } from '@xdefi-tech/chains-core';
+import {
+  GetArbitrumStatusDocument,
+  GetAuroraStatusDocument,
+  GetAvalancheStatusDocument,
+  GetCantoEvmStatusDocument,
+  GetCronosEvmStatusDocument,
+  GetEthereumStatusDocument,
+  GetFantomStatusDocument,
+  GetOptimismStatusDocument,
+  GetPolygonStatusDocument,
+  GetSmartChainStatusDocument,
+} from '@xdefi-tech/chains-graphql';
 
 import { EVMChains } from '../../../manifests';
 
-export const GET_STATUS = (chain: string) => gql`
-query GetStatus {
-  ${chain} {
-    status {
-      lastBlock
-    }
-  }
-}
-`;
-
 export const getStatus = async (chain: string) => {
   let indexerChain: string = chain;
+  let query: any;
+
   switch (chain) {
+    case EVMChains.ethereum:
+      query = GetEthereumStatusDocument;
+      break;
     case EVMChains.smartchain:
       indexerChain = 'binanceSmartChain';
+      query = GetSmartChainStatusDocument;
+      break;
+    case EVMChains.polygon:
+      query = GetPolygonStatusDocument;
+      break;
+    case EVMChains.avalanche:
+      query = GetAvalancheStatusDocument;
+      break;
+    case EVMChains.fantom:
+      query = GetFantomStatusDocument;
+      break;
+    case EVMChains.arbitrum:
+      query = GetArbitrumStatusDocument;
+      break;
+    case EVMChains.aurora:
+      query = GetAuroraStatusDocument;
       break;
     case EVMChains.cantoevm:
       indexerChain = 'cantoEVM';
+      query = GetCantoEvmStatusDocument;
       break;
+    case EVMChains.optimism:
+      query = GetOptimismStatusDocument;
+      break;
+    case EVMChains.cronos:
+      indexerChain = 'cronosEVM';
+      query = GetCronosEvmStatusDocument;
+      break;
+    default:
+      throw new Error(
+        `Unsupported chain: ${chain}. Please check the configuration.`
+      );
   }
+
   const response = await gqlClient.query({
-    query: GET_STATUS(indexerChain),
+    query: query,
   });
 
   return response.data[indexerChain].status;
