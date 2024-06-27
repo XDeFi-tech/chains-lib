0bcd050e52792acf1acf3586a0360ceef99a9e08
danilxdefi <109511564+danilxdefi@users.noreply.github.com>
2024-06-27 17:57:31 +0300

    feat: XDEFI-8304 thorchain nodeurl (#247)
    * feat: add indexer datasource to thorchain package (WIP)
    
    * feat: update manifest with nodeUrl, update testcases

---
+++ packages/thor/src/datasource/indexer/queries/balances.query.ts
@@ -0,0 +1,50 @@
+import { gqlClient } from '@xdefi-tech/chains-core';
+import {
+  GetMayachainBalancesDocument,
+  GetThorchainBalancesDocument,
+} from '@xdefi-tech/chains-graphql';
+
+import { ThorChains } from '../../../manifests';
+
+type ThorchainParams = {
+  query: any;
+  queryName: string;
+};
+
+const getChainParams = (chain: string): ThorchainParams => {
+  const params: ThorchainParams = {
+    query: null,
+    queryName: '',
+  };
+
+  const formattedChain = chain.toLowerCase();
+  switch (formattedChain) {
+    case ThorChains.mayachain:
+      params.query = GetMayachainBalancesDocument;
+      params.queryName = ThorChains.mayachain;
+      break;
+    case ThorChains.thorchain:
+      params.query = GetThorchainBalancesDocument;
+      params.queryName = ThorChains.thorchain;
+      break;
+  }
+
+  return params;
+};
+
+export const getBalance = async (chain: string, address: string) => {
+  const params = getChainParams(chain);
+
+  if (!params.query) {
+    throw new Error('Unsupported thor chain-like chain');
+  }
+
+  const response = await gqlClient.query({
+    query: params.query,
+    variables: {
+      address,
+    },
+  });
+
+  return response.data[params.queryName].balances;
+};
