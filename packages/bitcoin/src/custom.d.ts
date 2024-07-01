31c511df4cb3f27705f342d17105dee22d8c5b11
sotatek-tyler-nguyen4 <166351995+sotatek-tyler-nguyen4@users.noreply.github.com>
2024-07-01 23:05:01 +0700

    fix: calculate fee rate to use for bitcoin dusting algorithm (#256)
    * fix: calculate fee rate to use for bitcoin dusting algorthm
    
    * chore: update unit tests
    
    * feat: add changeset
    
    ---------
    
    Co-authored-by: sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>
    Co-authored-by: danilxdefi <danylo@xdefi.io>

--- packages/bitcoin/src/custom.d.ts
+++ packages/bitcoin/src/custom.d.ts
@@ -1,5 +1,6 @@
 declare module 'coinselect';
 declare module 'coinselect/accumulative';
+declare module 'coinselect/utils';
 declare module 'tiny-secp256k1/index.js';
 declare module 'hdkey';
 declare module 'coinkey';
