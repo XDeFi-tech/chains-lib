dff33a9373d1b991a8d21cdd80d4d2cfa34230c1
Liam McAweeney <59666496+liammcaweeney@users.noreply.github.com>
2024-07-08 16:35:55 +0100

    feat: added bundle analysis on react app (#267)

--- examples/react/next.config.js
+++ examples/react/next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
     reactStrictMode: true,
     webpack: function (config, options) {
         config.experiments = { asyncWebAssembly: true };
         return config;
     }
-};
+});
