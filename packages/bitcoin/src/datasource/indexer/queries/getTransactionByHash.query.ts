95bce5b22125251c3056d5a1505dc177d71634c7
sotatek-tyler-nguyen4 <166351995+sotatek-tyler-nguyen4@users.noreply.github.com>
2024-07-19 13:22:33 +0700

    feat: Ordinals - allow to spend UTXOs without losing the Ordinals (#231)
    * feat: Ordinals - allow to spend UTXOs without losing the Ordinals
    
    * fix: Add logic to spend UTXOs containing ordinals without losing the ordinals
    
    * fix: Add logic to spend UTXOs containing ordinals without losing the ordinals
    
    * chore: add logic to prevent creating bitcoin dust when splitting ordinals
    
    * feat: handle mutilple ordinals in a utxo and refactor code with new bitcoin dust filter
    
    * feat: handle mutilple ordinals in a utxo
    
    * feat: handle mutilple ordinals in a utxo
    
    * bump: add changeset
    
    ---------
    
    Co-authored-by: sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>
    Co-authored-by: danilxdefi <danylo@xdefi.io>

--- packages/bitcoin/src/datasource/indexer/queries/getTransactionByHash.query.ts
+++ packages/bitcoin/src/datasource/indexer/queries/getTransactionByHash.query.ts
@@ -1,11 +1,34 @@
 import { gqlClient } from '@xdefi-tech/chains-core';
+import { gql } from 'graphql-tag';
 
 import { BitcoinGetTransactionByHashDocument } from '../../../gql/graphql';
+export const BITCOIN_GET_TRANSACTION_BY_HASH = gql`
+  query GetTransactionByHash($txHash: String!) {
+    bitcoin {
+      getTransactionByHashV5(txHash: $txHash) {
+        hash
+        outputs {
+          address
+          amount {
+            value
+          }
+        }
+        inputs {
+          address
+          amount {
+            value
+          }
+        }
+        blockNumber
+      }
+    }
+  }
+`;
 
 export const getTransactionByHash = async (txHash: string) => {
   try {
     const response = await gqlClient.query({
-      query: BitcoinGetTransactionByHashDocument,
+      query: BITCOIN_GET_TRANSACTION_BY_HASH,
       variables: {
         txHash,
       },
