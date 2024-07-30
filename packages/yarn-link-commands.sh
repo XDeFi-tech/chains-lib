db9785b505e1d7076b0d3f9d1768b879239ea0c2
Liam McAweeney <59666496+liammcaweeney@users.noreply.github.com>
2024-07-30 11:42:11 +0100

    @szymon/tsup node (#282)
    * add link-all-directories.sh
    
    * yarn-link-commands.sh
    
    * fix
    
    * add unlink
    
    * add link-all-directories.sh
    
    * yarn-link-commands.sh
    
    * fix
    
    * add unlink
    
    * use tsup but excluse packages
    
    * cleanup
    
    * bump: add changeset
    
    ---------
    
    Co-authored-by: Szymon Kapała <szymon20000@gmail.com>
    Co-authored-by: danilxdefi <danylo@xdefi.io>
    Co-authored-by: dp <25910069+davidp94@users.noreply.github.com>

---
+++ packages/yarn-link-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yarn link @xdefi-tech/chains-binance
+yarn link @xdefi-tech/chains-bitcoin
+yarn link @xdefi-tech/chains-bitcoincash
+yarn link @xdefi-tech/chains-core
+yarn link @xdefi-tech/chains-cosmos
+yarn link @xdefi-tech/chains-dogecoin
+yarn link @xdefi-tech/chains-evm
+yarn link @xdefi-tech/chains-litecoin
+yarn link @xdefi-tech/chains-solana
+yarn link @xdefi-tech/chains-thor
+yarn link @xdefi-tech/chains-tron
+yarn link @xdefi-tech/chains-utxo
---
+++ packages/yarn-unlink-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yarn unlink @xdefi-tech/chains-binance
+yarn unlink @xdefi-tech/chains-bitcoin
+yarn unlink @xdefi-tech/chains-bitcoincash
+yarn unlink @xdefi-tech/chains-core
+yarn unlink @xdefi-tech/chains-cosmos
+yarn unlink @xdefi-tech/chains-dogecoin
+yarn unlink @xdefi-tech/chains-evm
+yarn unlink @xdefi-tech/chains-litecoin
+yarn unlink @xdefi-tech/chains-solana
+yarn unlink @xdefi-tech/chains-thor
+yarn unlink @xdefi-tech/chains-tron
+yarn unlink @xdefi-tech/chains-utxo
