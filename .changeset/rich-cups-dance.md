8b8bef6382adac933d797c58f86bf3eaffb5e6b4
sotatek-tyler-nguyen4 <166351995+sotatek-tyler-nguyen4@users.noreply.github.com>
2024-08-12 15:46:54 +0700

    fix: error in fee estimation (#299)
    * fix: error in fee estimation
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>

---
+++ .changeset/rich-cups-dance.md
@@ -0,0 +1,5 @@
+---
+'@xdefi-tech/chains-cosmos': patch
+---
+
+fix: error in fee estimation
--- packages/cosmos/src/chain.provider.spec.ts
+++ packages/cosmos/src/chain.provider.spec.ts
@@ -2,6 +2,7 @@ import {
   Response,
   GasFeeSpeed,
   TransactionStatus,
+  MsgEncoding,
 } from '@xdefi-tech/chains-core';
 
 import { ChainMsg } from './msg';
@@ -108,26 +109,31 @@ describe('chain.provider', () => {
     expect(balanceData.length).toEqual(2);
   });
 
-  it('estimateFee() should return fee estimation', async () => {
-    jest.spyOn(CosmosProvider.prototype, 'estimateFee').mockResolvedValue([
+  it('estimatFee() should return fee estimation with encoding string msg', async () => {
+    const osmosisProvider = new CosmosProvider(
+      new IndexerDataSource(COSMOS_MANIFESTS.osmosis)
+    );
+    const msg = osmosisProvider.createMsg(
       {
-        gasLimit: 1,
-        gasPrice: 1,
-        maxFeePerGas: 1,
-        baseFeePerGas: 1,
-        maxPriorityFeePerGas: 1,
+        data: '{"signDoc":{"accountNumber":"2551461","chainId":"osmosis-1","fee":{"gas":"318939","amount":[{"amount":"1196","denom":"uosmo"}]},"memo":"FE","msgs":[{"typeUrl":"/osmosis.gamm.v1beta1.MsgSwapExactAmountIn","value":{"routes":[{"poolId":"1400","tokenOutDenom":"ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"}],"sender":"osmo1nvt0fx864yyuyjvpw7eh2uj5zudcfkcn8ra5mf","tokenIn":{"amount":"5000000","denom":"uosmo"},"tokenOutMinAmount":"370642"}}],"sequence":"6","timeoutHeight":"18897301"},"signer":"osmo1nvt0fx864yyuyjvpw7eh2uj5zudcfkcn8ra5mf"}',
       },
-    ]);
+      MsgEncoding.string
+    );
+    const estimateFee = await osmosisProvider.estimateFee(
+      [msg],
+      GasFeeSpeed.low
+    );
+    expect(estimateFee[0].gasLimit).toBeTruthy();
+  });
 
+  it('estimateFee() should return fee estimation with encoding json msg', async () => {
     const msg = provider.createMsg({
-      from: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
-      to: 'cosmos1rysuxnc2qdedfxpwj4g26a59yr53kxzd2r7yd4',
+      from: 'cosmos1nvt0fx864yyuyjvpw7eh2uj5zudcfkcn0cwydm',
+      to: 'cosmos1nvt0fx864yyuyjvpw7eh2uj5zudcfkcn0cwydm',
       amount: 0.000001,
     });
 
     const estimateFee = await provider.estimateFee([msg], GasFeeSpeed.medium);
-
-    expect(estimateFee.length).toEqual(1);
     expect(estimateFee[0].gasLimit).toBeTruthy();
   });
 
@@ -174,6 +180,15 @@ describe('chain.provider', () => {
   });
 
   it('getFeeTokens(): should get a list of fee tokens', async () => {
+    jest.spyOn(CosmosProvider.prototype, 'estimateFee').mockResolvedValue([
+      {
+        gasLimit: 1,
+        gasPrice: 1,
+        maxFeePerGas: 1,
+        baseFeePerGas: 1,
+        maxPriorityFeePerGas: 1,
+      },
+    ]);
     jest.setTimeout(30000);
     const feeTokens = await provider.getFeeTokens();
     expect(Array.isArray(feeTokens)).toBe(true);
