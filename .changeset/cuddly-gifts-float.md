ea45e83379cc5b36459a612d34b123822f0219fc
Liam McAweeney <59666496+liammcaweeney@users.noreply.github.com>
2024-10-09 12:07:24 +0100

    chore: increased default contract fee from 100k to 1mill (#400)

---
+++ .changeset/cuddly-gifts-float.md
@@ -0,0 +1,5 @@
+---
+'@xdefi-tech/chains-evm': patch
+---
+
+chore: increased default contract fee from 100k to 1mill
--- packages/evm/src/constants.ts
+++ packages/evm/src/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_TRANSACTION_FEE = 21000;
-export const DEFAULT_CONTRACT_FEE = 100000;
+export const DEFAULT_CONTRACT_FEE = 1000000;
 export const ERC721_SAFE_TRANSFER_METHOD =
   'safeTransferFrom(address,address,uint256)';
 export const ERC1155_SAFE_TRANSFER_METHOD =
