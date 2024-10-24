cd5c67d11c2ae1601b79630683b4a72e52656d6a
sotatek-tyler-nguyen4 <166351995+sotatek-tyler-nguyen4@users.noreply.github.com>
2024-10-24 14:42:20 +0700

    Test apollo changes (#348)
    * chore: test apollo changes
    
    * chore: test apollo changes
    
    * fix: use compound key instead of address, clean logs
    
    ---------
    
    Co-authored-by: sotatek-tyler-nguyen4 <tyler.nguyen4@gmail.com>
    Co-authored-by: danilxdefi <danylo@xdefi.io>

--- packages/litecoin/src/datasource/indexer/queries/fees.query.ts
+++ packages/litecoin/src/datasource/indexer/queries/fees.query.ts
@@ -5,7 +5,6 @@ import { GetLitecoinFeesDocument } from '../../../gql/graphql';
 export const getFees = async (): Promise<DefaultFeeOptions> => {
   const response = await gqlClient.query({
     query: GetLitecoinFeesDocument,
-    fetchPolicy: 'no-cache',
   });
 
   return response?.data.litecoin.fee as DefaultFeeOptions;
