5fa3c93c766b2e1d70f0d705d11a743ad45f8de4
danilxdefi <109511564+danilxdefi@users.noreply.github.com>
2024-12-02 18:29:26 +0200

    feat: add all broadcast options to solana package (#478)

--- packages/solana/src/constants.ts
+++ packages/solana/src/constants.ts
@@ -1,1 +1,14 @@
 export const DEFAULT_FEE = 5000;
+
+export interface BroadcastOptions {
+  skipPreflight?: boolean;
+  preflightCommitment?: 'processed' | 'confirmed' | 'finalized';
+  maxRetries?: number;
+  minContextSlot?: number;
+}
+
+export const DEFAULT_BROADCAST_OPTIONS: BroadcastOptions = {
+  skipPreflight: false,
+  preflightCommitment: 'confirmed',
+  maxRetries: 0,
+};
