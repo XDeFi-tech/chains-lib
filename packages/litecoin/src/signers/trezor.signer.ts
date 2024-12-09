f2cde2d6420ffad2a86d32648ee8b7ce8f4c2bae
Amin Limbada <aminlimbada94@gmail.com>
2024-12-09 12:39:46 +0400

    fix: nput does not match scriptPubKey error

--- packages/litecoin/src/signers/trezor.signer.ts
+++ packages/litecoin/src/signers/trezor.signer.ts
@@ -47,10 +47,24 @@ export class TrezorSigner extends Signer.TrezorProvider {
       .split('/')
       .map(Number);
 
+    let scriptType: string;
+    if (derivationArray[0] === 84) {
+      scriptType = 'SPENDWITNESS';
+    } else if (derivationArray[0] === 49) {
+      scriptType = 'SPENDP2SHWITNESS';
+    } else if (derivationArray[0] === 86) {
+      scriptType = 'SPENDTAPROOT';
+    } else if (derivationArray[0] === 44) {
+      scriptType = 'SPENDADDRESS';
+    } else {
+      throw new Error(`Unsupported path type: ${derivationArray[0]}`);
+    }
+
     const inputs = txData.inputs.map((utxo: UTXO) => ({
       prev_hash: utxo.hash,
       prev_index: utxo.index,
       amount: utxo.value,
+      script_type: scriptType,
       address_n: [
         (derivationArray[0] | 0x80000000) >>> 0,
         (derivationArray[1] | 0x80000000) >>> 0,
