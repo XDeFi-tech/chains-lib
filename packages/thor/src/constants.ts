27b826889b2c3e512883fe93f0969ec0a76872d2
danilxdefi <109511564+danilxdefi@users.noreply.github.com>
2024-12-30 20:31:48 +0200

    feat: revert migrate deposit to via send for thorchain txs (#501)

--- packages/thor/src/constants.ts
+++ packages/thor/src/constants.ts
@@ -1,5 +1,2 @@
 export const NATIVE_TRON_FEE = 2_000_000; // TOR 0.02 RUNE https://thornode.ninerealms.com/thorchain/constants NativeTransactionFee
 export const NATIVE_MAYA_FEE = 2_000_000_000; // TOR 0.2 CACAO https://mayanode.mayachain.info/mayachain/mimir NativeTransactionFee
-
-export const DEPOSIT_MSG_ADDRESS =
-  'thor1v8ppstuf6e3x0r4glqc68d5jqcs2tf38cg2q6y'; // address for deposit via send, https://gitlab.com/thorchain/thornode/-/tree/aac621ac97889d5525a67b210c7ac3a2e00f20e9/app/upgrades/v3#new-transaction-types
