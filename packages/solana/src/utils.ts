739b2d136012a5300e2b04c6d4a86746664c62b3
danilxdefi <109511564+danilxdefi@users.noreply.github.com>
2025-01-06 20:15:36 +0200

    feat: remove @solana-developers/helpers dependency (#508)

--- packages/solana/src/utils.ts
+++ packages/solana/src/utils.ts
@@ -1,8 +1,15 @@
 import { TransactionStatus } from '@xdefi-tech/chains-core';
 import {
-  ComputeBudgetProgram,
   Connection,
+  PublicKey,
+  TransactionInstruction,
+  AddressLookupTableAccount,
   VersionedTransaction,
+  TransactionMessage,
+  ComputeBudgetProgram,
+  RpcResponseAndContext,
+  SimulatedTransactionResponse,
+  TransactionError,
 } from '@solana/web3.js';
 
 export const checkMinimumBalanceForRentExemption = async (
@@ -62,4 +69,59 @@ export const checkTxAlreadyHasPriorityFee = (txBytes: Buffer) => {
     (key) => key.toBase58() === ComputeBudgetProgram.programId.toBase58()
   );
   return !!priorityFee;
+};
+
+type TransactionCustomError = TransactionError & {
+  InstructionError: [number, { Custom: string }];
+};
+
+const getErrorFromRPCResponse = (
+  rpcResponse: RpcResponseAndContext<SimulatedTransactionResponse>
+) => {
+  const error = rpcResponse.value.err as TransactionCustomError | null;
+  if (error) {
+    if (typeof error === 'object') {
+      const errorKeys = Object.keys(error);
+      if (errorKeys.length === 1) {
+        if (errorKeys[0] !== 'InstructionError') {
+          throw new Error(`Unknown RPC error: ${error}`);
+        }
+        const instructionError = error.InstructionError;
+        throw new Error(
+          `Error in transaction: instruction index ${instructionError[0]}, custom program error ${instructionError[1]['Custom']}`
+        );
+      }
+    }
+    throw Error(error.toString());
+  }
+};
+
+export const getSimulationComputeUnits = async (
+  connection: Connection,
+  instructions: Array<TransactionInstruction>,
+  payer: PublicKey,
+  lookupTables: Array<AddressLookupTableAccount> | []
+): Promise<number | null> => {
+  const testInstructions = [
+    // Set an arbitrarily high number in simulation
+    // so we can be sure the transaction will succeed
+    // and get the real compute units used
+    ComputeBudgetProgram.setComputeUnitLimit({ units: 14e5 }),
+    ...instructions,
+  ];
+  const testTransaction = new VersionedTransaction(
+    new TransactionMessage({
+      instructions: testInstructions,
+      payerKey: payer,
+      // RecentBlockhash can by any public key during simulation
+      // since 'replaceRecentBlockhash' is set to 'true' below
+      recentBlockhash: PublicKey.default.toString(),
+    }).compileToV0Message(lookupTables)
+  );
+  const rpcResponse = await connection.simulateTransaction(testTransaction, {
+    replaceRecentBlockhash: true,
+    sigVerify: false,
+  });
+  getErrorFromRPCResponse(rpcResponse);
+  return rpcResponse.value.unitsConsumed || null;
 };
